98bc5f2bf074dfcc90153614bc55384dbaf2a089
hermit699 <104385313+hermit699@users.noreply.github.com.>
2026-05-26 10:09:57 +0545

    Fresh start: Added Javascript folder with operators code

---
+++ Javascript/operators.js
@@ -0,0 +1,50 @@
+/* comparison operator 
+Comaparison operator includes the following:
+- == (equal to)
+- === (strictly equal to)
+- != (not equal to)
+- !== (strictly not equal to)
+- > (greater than)
+- < (less than)
+- >= (greater than or equal to)
+- <= (less than or equal to)
+*/
+// let a = "10";
+// let b = 10;
+// console.log(a==b);
+// console.log(a===b);
+// console.log(a!=b);
+// console.log(a!==b);
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a>=b);
+// console.log(a<=b);
+
+// // // assignment operator
+
+// let x = 10;
+// let y = 20;
+// x +=y;
+// x -=y;
+// console.log(x);
+// console.log(x);
+
+//logical Operators 
+ // ! AND [&& ] OR [||] NOT [!]
+
+let w = true;
+let z = false;
+console.log( w && z) ;
+console.log(w || z);
+console.log(!w);
+console.log(!z);
+
+
+
+
+
+
+
+
+
+
