c9a5ab168a360436adab41279532f3b20f167b3c
hermit699 <104385313+hermit699@users.noreply.github.com.>
2026-05-26 10:13:46 +0545

    updated form of operators.js

--- Javascript/operators.js
+++ Javascript/operators.js
@@ -9,25 +9,25 @@ Comaparison operator includes the following:
 - >= (greater than or equal to)
 - <= (less than or equal to)
 */
-// let a = "10";
-// let b = 10;
-// console.log(a==b);
-// console.log(a===b);
-// console.log(a!=b);
-// console.log(a!==b);
-// console.log(a>b);
-// console.log(a<b);
-// console.log(a>=b);
-// console.log(a<=b);
+let a = "10";
+let b = 10;
+console.log(a==b);
+console.log(a===b);
+console.log(a!=b);
+console.log(a!==b);
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+console.log(a<=b);
 
-// // // assignment operator
+// assignment operator
 
-// let x = 10;
-// let y = 20;
-// x +=y;
-// x -=y;
-// console.log(x);
-// console.log(x);
+let x = 10;
+let y = 20;
+x +=y;
+x -=y;
+console.log(x);
+console.log(x);
 
 //logical Operators 
  // ! AND [&& ] OR [||] NOT [!]
