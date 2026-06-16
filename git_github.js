50aacd7bd3435558a76665739855d28fb1530c55
hermit699 <104385313+hermit699@users.noreply.github.com>
2026-06-16 07:24:55 +0545

    Add git_github.js script

---
+++ git_github.js
@@ -0,0 +1,24 @@
+//* Git commands
+ 
+/* git config --global --list 
+/* git config --global user.name "<username>"
+/* git config --global user.email
+/* git config --global init.defaultbranch "main"
+
+
+*/
+
+//? initialize local git repo:  git init 
+
+
+//?  git working flow
+//working directory  -> staging area  ---> local repo 
+// local changes                 ready-state     new version 
+
+ 
+
+
+
+
+
+
