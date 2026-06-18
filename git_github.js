11ee8b39b17acfc38185cafeae214c092c97138d
hermit699 <104385313+hermit699@users.noreply.github.com>
2026-06-18 07:01:36 +0545

    brancing commands

--- git_github.js
+++ git_github.js
@@ -3,7 +3,7 @@
 /* git config --global --list 
 /* git config --global user.name "<username>"
 /* git config --global user.email
-/* git config --global init.defaultbranch "main"
+/* git config --global init.default branch "main"
 
 
 */
@@ -15,10 +15,30 @@
 //working directory  -> staging area  ---> local repo 
 // local changes                 ready-state     new version 
 
+ //? git status
+ //* git add <file_path> 
+ //* git commit -m <"commit_messge"
+ 
+ 
+
+ //? git remote -v 
+
+ // git remote add origin <remote_repo_url> 
+ // git remote remove origin 
+ // git push origin <branch_name> 
+
+ //? ignore 
+ // .gitignore file
+
+ //branching 
+
+ // git branch -> List all local branches
+ // git branch <branch_name>   -> create new branch 
+
  
 
 
-
+ 
 
 
 
